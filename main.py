99fe0e5d17efbb60bf09767b4d7720bee28bc91c
Karen Ñanco <k.nanco.vasquez@gmail.com>
2026-03-05 01:08:32 +0000

    prueba python

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+print ("hola wiwiwiwiwiw")
+
+# variables:
+
+nombre = "karen"
+edad = 35
+altura = 1.67
+es_profesor = True
+tiene_perro = False
+ciudad = "osorno"
+genero = ["hombre", "mujer", "therian"]
+
+
+#TIPOS DE DATOS
+
+str #esto es un texto
+int #esto es un entero
+float #esto es un flotante
+bool #esto es verdadero o falso
+
+#INPUT
+#input()
+
+
+texto = input()
+print (texto)
+
+
