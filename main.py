640bd7e4f0b17255154ff0de892cac11e724a2d2
karenanco <k.nanco.vasquez@gmail.com>
2026-03-05 22:06:12 -0300

    pyhton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-print ("hola wiwiwiwiwiw")
+
 
 # variables:
 
@@ -23,8 +23,57 @@ bool #esto es verdadero o falso
 #INPUT
 #input()
 
+#numero = int (input ("escribe un numero : "))
+#resultado = numero + 1
+#print(resultado)
 
-texto = input()
-print (texto)
+#INPUT
+""" texto = input("escribe un precio : ") #pedir por consola un dato tipo
+precio = int (precio) #convertir a entero el str con int (precio)
+precio = precio + 10 #el precio sumado + 10 ya que ahora es un numero entero 
+print (precio) #imprime el precio en pantalla """
+
+## CONDICIONALES
 
 
+#if condicion:
+#  codigo
+
+""" edad = 15
+
+if edad >= 18:
+    print ("puede entrar a la disco")
+
+elif edad >=50: 
+    print ("demasiado antiguo")
+else: 
+    print ("no puede entrar a la disco") """
+
+#BUCLES
+
+#WHILE
+#while condicion:
+#   codigo
+# while significa "mientras pase algo"
+
+""" contador = 0
+
+while contador < 10:
+    print (contador)
+    contador += 1
+
+while True:
+    texto = input ("escribe salir para salir del bucle")
+    if texto == "salir":
+        break """
+
+
+#FOR
+
+#for elemento in lista
+#   codigo
+
+lista = [1,2,3,4,5,6,7,8,9]
+
+for i in lista:
+    print (i)
