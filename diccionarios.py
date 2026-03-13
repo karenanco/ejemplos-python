f0541d1e392432fc20a4469e15dea541a1e7cdea
karenanco <k.nanco.vasquez@gmail.com>
2026-03-12 22:20:08 -0300

    ejemplo

--- diccionarios.py
+++ diccionarios.py
@@ -81,3 +81,89 @@ while True:
     else:
         print("Opcion no correcta, solo se acepta 1,2 y 3 programa cerrado")
 
+# Proyecto de lista de películas netflix
+
+pelicula = {
+    nombre : nombre
+    anio : anio
+    actores : [actor1,actor2...]
+}
+
+# peliculas=[{pelicula1},{pelicula2}...]
+# int()  
+
+#flujo del programa
+'''
+generar lista vacia
+peliculas=[]
+imprimir
+-----Menú-----
+1. Agregar
+2. Borrar
+3. Ver
+4. Salir
+
+que el usuario entregue la opción
+
+si el usuario eligió 1
+        ingresar nombre de la pelicula
+        ingresar anio de la pelicula
+        ingresar género
+        pelicula = {
+            nombre : nombre
+            anio :anio
+            actores : [actor1,actor2...]
+            }
+        agregar la película a la lista (append)
+        mensaje que la película está guardada
+
+si el usuario eligió 2
+        ingresar la pelicula a borrar
+        buscamos la pelicula a borrar
+        borramos
+
+si el usuario eligió 3
+        ve el listado
+
+si el usuario eligió 4
+        mensaje de salida
+        break
+
+si el usuario puso otra cosa, el progama se cierra
+
+'''
+
+peliculas = []
+
+while True:
+    print("-----------Menú----------")
+    print("1. Agregar")
+    print("2. Borrar")
+    print("3. Ver")
+    print("4. Salir")
+
+    opcion = input(ingrese la opcion)
+
+    if opcion == "1":
+        nombre = input("Ingresar nombre de la película")
+        nombre = input("Ingresar anio de la película")
+        nombre = input("Ingresar género de la película")
+
+        pelicula = {
+            "nombre" : nombre,
+            "anio" :anio,
+            "genero" : genero
+            }
+        peliculas.append(pelicula)
+        print("la pelicula esta guardada")
+    elif opcion == "2":
+        peli_borrar = input("ingresa la peli a borrar")
+
+        for peli in peliculas:
+
+            if peli["nombre"] == peli_borrar:
+    elif opcion == "3":
+    elif opcion == "4":
+    else: 
+        print("opcion no valida")
+    
