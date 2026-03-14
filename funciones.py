88d71fcbabe6938e8ccf688c0d0b29696f07b029
karenanco <k.nanco.vasquez@gmail.com>
2026-03-13 21:52:54 -0300

    funciones

---
+++ funciones.py
@@ -0,0 +1,13 @@
+""" def saludar():
+    print("hola esta es mi primera funcion!!")
+
+saludar() #invoke """
+
+
+def sumar(numero1, numero2):
+    resultado = numero1 + numero2
+    return resultado
+
+ver = sumar(3,8)
+
+print(ver)
